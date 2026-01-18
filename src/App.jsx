56f2962ce851055d630c4713229ae87f196afbb4
Aman Ghira <ghiraaman@gmail.com>
2026-01-19 02:28:16 +0530

    Experiment 3: Theme Toggle SPA

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import Counter from "./components/Counter";
 import Todo from "./components/Todo";
+import ThemeToggle from "./components/ThemeToggle";
+import Form from "./components/Form";
 
 function App() {
   const [page, setPage] = useState("counter");
@@ -9,9 +11,13 @@ function App() {
     <div style={{ padding: "20px" }}>
       <button onClick={() => setPage("counter")}>Counter</button>
       <button onClick={() => setPage("todo")}>To-Do</button>
+      <button onClick={() => setPage("theme")}>Theme Toggle</button>
+      <button onClick={() => setPage("form")}>Form</button>
 
       {page === "counter" && <Counter />}
       {page === "todo" && <Todo />}
+      {page === "theme" && <ThemeToggle />}
+      {page === "form" && <Form />}
     </div>
   );
 }
